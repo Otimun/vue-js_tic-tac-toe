6efb5e40e241f76b2bf8c27f066b9247ceb61c1a
Otimun <michael@mictu.net>
2023-08-25 14:55:39 -0400

    adding tic-tac-toe board to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
-
+  import Tile from '@/components/Tile.vue'
+  import Board from '@/components/Board.vue'
+  const board_size = 3
+  const tiles = Array.from({ length: board_size * board_size }, 
+      (_, index) => ({ id: index }))
 </script>
 
 <template>
-    <div>Home</div>
+  <div id="app">
+    <Board :size=board_size :tiles=tiles />
+  </div>
 </template>
+
