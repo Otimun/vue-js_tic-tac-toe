7392ba8b058a96b94ab1261e0046735e727036e6
Otimun <michael@mictu.net>
2023-08-25 11:39:16 -0400

    add a different reference to the component

--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import Header from '../Header.vue'
+import Header from '@/components/Header.vue'
 
 describe('Header', () => {
   it('renders a header', () => {
